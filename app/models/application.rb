5418c8514d76f51ff576f44e76f1d644e7e183bc
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-20 19:09:58 -0600

    Submit app turns status to pending

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,16 +1,11 @@
 class Application < ApplicationRecord
-  before_save :set_default_val
   validates :name, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true, numericality: true, length: { is: 5 }
-  # validates :applicant_bio
-  # validates :application_status
+  validate :applicant_bio
+  validate :application_status
   has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
-
-  def set_default_val
-    self.application_status = 'In Progress'
-  end
 end
